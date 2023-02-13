6f3c40465582fa06f7f35e176d1505a503e5f83b
Victor Matias <fvictor1705@gmail.com>
2023-02-13 16:58:19 -0300

    feat: create CarODM model

---
+++ src/Models/CarODM.ts
@@ -0,0 +1,29 @@
+import { Schema, Model, models, model } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+class CarODM {
+  private _schema: Schema;
+  private _model: Model<ICar>;
+
+  constructor() {
+    this._schema = new Schema<ICar>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+
+    this._model = models.Car || model('Car', this._schema);
+  }
+
+  public async createCar(car: ICar): Promise<ICar> {
+    const newCar = await this._model.create(car);
+
+    return newCar;
+  }
+}
+
+export default CarODM;
