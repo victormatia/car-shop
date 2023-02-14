2776d7ffd583f82688c6d48445233a41b18046a5
Victor Matias <fvictor1705@gmail.com>
2023-02-14 15:47:13 -0300

    feat: add get routes for '/cars' and '/cars/:id' endpoints

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import CarController from '../Controllers/CarController';
+import ObjectId from '../Middlewares/isValidObjetcId';
 import CarODM from '../Models/CarODM';
 import CarService from '../Services/CarService';
 
 const route = Router();
 
 route.post('/', new CarController(new CarService(new CarODM())).createCar);
+route.get('/', new CarController(new CarService(new CarODM())).getAllCars);
+route.get('/:id', ObjectId.validate, new CarController(new CarService(new CarODM())).getCarById);
 
 export default route;
