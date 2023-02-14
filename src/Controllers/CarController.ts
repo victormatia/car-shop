513748d8082499ccf948d41aba647204c8b4fabb
Victor Matias <fvictor1705@gmail.com>
2023-02-14 15:42:02 -0300

    feat: adds getAllCars and getCarById methods to CarController class

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -11,6 +11,21 @@ class CarController {
 
     res.status(201).json(addedCar);
   };
+
+  public getAllCars: RequestHandler = async (_req, res) => {
+    const allCars = await this._service.getAllCars();
+
+    res.status(200).json(allCars);
+  };
+
+  public getCarById: RequestHandler = async (req, res) => {
+    const { id } = req.params;
+    const { message, result } = await this._service.getCarById(id);
+
+    if (message) return res.status(404).json({ message });
+
+    res.status(200).json(result);
+  };
 }
 
 export default CarController;
