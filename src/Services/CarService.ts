ea1b1ac7fa10a2591d1765b796ab80d3aae7a4df
Victor Matias <fvictor1705@gmail.com>
2023-02-14 15:41:05 -0300

    feat: adds getAllCars and getCarById methods to CarService class

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,5 +1,6 @@
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
+import IServiceReturn from '../Interfaces/IServiceReturn';
 import CarODM from '../Models/CarODM';
 
 class CarService {
@@ -7,10 +8,24 @@ class CarService {
     this._model = _model;
   }
 
-  public async createCar(newCar: ICar) {
+  public async createCar(newCar: ICar): Promise<Car> {
     const addedCar = await this._model.createCar(newCar);
 
     return new Car(addedCar);
+  }
+
+  public async getAllCars(): Promise<Car[]> {
+    const allCars = await this._model.getAllCars();
+
+    return allCars.map((car) => new Car(car));
+  }
+
+  public async getCarById(id: string): Promise<IServiceReturn> {
+    const car = await this._model.getCarById(id);
+
+    if (car) return { result: new Car(car) };
+
+    return { message: 'Car not found' };
   }
 }
 
