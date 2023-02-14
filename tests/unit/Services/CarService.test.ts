ed5932b3c74d112d97318e059594265c83c6b6f1
Victor Matias <fvictor1705@gmail.com>
2023-02-14 17:42:24 -0300

    test: implement a new test case for CarService

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -39,4 +39,42 @@ describe('Casos de testes de CarService', function () {
     // Assertion
     expect(result).to.be.deep.equal(output);
   });
+
+  it(
+    'Verifica se é possível listar todos os carros cadastrados no banco de dados',
+    async function () {
+    // Arrange
+      const output = [
+        new Car({
+          model: 'Mareuuuu',
+          year: 2002,
+          color: 'Black',
+          status: true,
+          buyValue: 15.99,
+          doorsQty: 4,
+          seatsQty: 5,
+          id: '63ebdc573f9ad2a53703c7a1',
+        }),
+        new Car({
+          model: 'Ford Mustang',
+          year: 1978,
+          color: 'Black',
+          status: true,
+          buyValue: 23.00,
+          doorsQty: 2,
+          seatsQty: 4,
+          id: '63ebdc573f9ad2a53703c7a1',
+        }),
+      ];
+
+      stub(Model, 'find').resolves(output);
+
+      // Act
+      const service = new CarService(new CarODM());
+      const result = await service.getAllCars();
+
+      // Assertion
+      expect(result).to.be.deep.equal(output);
+    },
+  );
 });
