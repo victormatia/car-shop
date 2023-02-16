fb57ea2694a94f273c272cd895031e8e6c13f857
Victor Matias <fvictor1705@gmail.com>
2023-02-15 22:57:37 -0300

    test: adds two new test cases
    comment: checks if it is possible to list all the bikes and, when passed an id, if it is possible to get the information of one.

--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -44,7 +44,69 @@ describe('Casos de testes de MotorcycleService', function () {
     expect(result).to.be.deep.equal(output);
   });
 
-  it('', async function () {
-    expect(1).to.be.equal(1);
-  });
+  it(
+    'Verifica se é possível listar todas as motos cadastrados no banco de dados',
+    async function () {
+    // Arrange
+
+      const output = [
+        new Motorcycle({
+          id: '63ed8006c3d4c3bdfd688d83',
+          model: 'Honda Cb 600f Hornets',
+          year: 2005,
+          color: 'Yellow',
+          status: true,
+          buyValue: 30,
+          category: 'Street',
+          engineCapacity: 600,
+        }),
+        new Motorcycle({
+          id: '63ed8088c3d4c3bdfd688d85',
+          model: 'Honda Bros 160',
+          year: 2020,
+          color: 'Black',
+          status: true,
+          buyValue: 15,
+          category: 'Trail',
+          engineCapacity: 160,
+        }),
+      ];
+
+      stub(Model, 'find').resolves(output);
+
+      // Act
+      const service = new MotorcycleService(new MotorcycleODM());
+      const result = await service.getAllMotorcycles();
+
+      // Assertion
+      expect(result).to.be.deep.equal(output);
+    },
+  );
+
+  it(
+    'Verifica se é possível listar uma moto que foi cadastrada no banco de dados',
+    async function () {
+    // Arrange
+
+      const output = new Motorcycle({
+        id: '63ed8088c3d4c3bdfd688d85',
+        model: 'Honda Bros 160',
+        year: 2020,
+        color: 'Black',
+        status: true,
+        buyValue: 15,
+        category: 'Trail',
+        engineCapacity: 160,
+      });
+
+      stub(Model, 'findById').resolves(output);
+
+      // Act
+      const service = new MotorcycleService(new MotorcycleODM());
+      const { result } = await service.getMotorcycleById('63ed8088c3d4c3bdfd688d85');
+
+      // Assertion
+      expect(result).to.be.deep.equal(output);
+    },
+  );
 });
